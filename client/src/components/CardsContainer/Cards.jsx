94788e9a0cc12ec176375d8144573407c882f510
Crz9410 <crisstofer_9410@hotmail.com>
2023-05-23 23:56:21 -0500

    pruebas

--- client/src/components/CardsContainer/Cards.jsx
+++ client/src/components/CardsContainer/Cards.jsx
@@ -8,14 +8,16 @@ const CardsContainer = ({ currentRecipes }) => {
       {currentRecipes?.map((recipe) => (
         <div className={Style.card} key={recipe.id}>
           {(
+            
             <Card
               id={recipe.id}
               name={recipe.title || recipe.name || "No hay información"}
               image={recipe.image || "No hay información"}
               summary={recipe.summary || "No hay información"}
-              healthy={recipe.healthScore || "No hay información"}
-              steps={recipe?.analyzedInstructions?.[0]?.steps || recipe?.steps || "No hay información"}
-            />
+              healthy={recipe.healthScore ||recipe.healthy ||"No hay información"}
+              steps={ recipe?.analyzedInstructions?.[0]?.steps  ||(typeof recipe?.steps === "string" ? recipe?.steps?.split("") : recipe?.steps ) }
+            /> 
+            // console.log("MMMMMMMMMMMMMM", recipe)
           )}
         </div>
       ))}
--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Detail.module.css";
 
 const Detail = (props) => {
   const { id, name, image, healthy, summary, steps } = props.location.state;
-
+  console.log("FFFFFFFF", steps)
+  
+  const handleImageError = (e) => {
+    e.target.src = "https://spoonacular.com/recipeImages/715497-312x231.jpg"; // Imagen por defecto
+  };
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>Receta Detallada</h2>
@@ -14,7 +18,8 @@ const Detail = (props) => {
         <p>
           <strong>Nombre:</strong> {name}
         </p>
-        <img src={image} alt="Not found" className={styles.image} />
+        <img src={image} alt="Not found" onError={handleImageError} className={styles.image} />
+
         <p>
           <strong>Healthy:</strong> {healthy}
         </p>
@@ -22,17 +27,29 @@ const Detail = (props) => {
           <strong>Summary:</strong>{" "}
           <div dangerouslySetInnerHTML={{ __html: summary }}></div>
         </p>
-        <p>
-          <strong>Steps:</strong>
-        </p>
+      
         <div className={styles.stepsContainer}>
-          {steps?.map((step) => (
-            <div className={styles.step} key={step.number}>
-              <div>
-                <strong>Paso {step.number}:</strong> {step.step}
-              </div>
-            </div>
-          ))}
+          {
+            typeof steps?.[0] !== "object" ? (
+              <>
+                <strong>Paso 1:</strong> {steps?.join("")}
+              </>
+            ) : (
+              <>
+                {steps?.map((step) => (
+                  <div className={styles.step} key={step.number}>
+                    <div>
+                      <>
+                        <strong>Paso {step.number}:</strong> {step.step}
+                      </>
+                    </div>
+                  </div>
+                ))}
+              </>
+            )
+          }
+
+
         </div>
       </div>
     </div>
